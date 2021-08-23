68bc4ef75710072aa97a096ccc6942601122cd42
lemao-code <th.ferrari@outlook.com.br>
2021-08-23 18:22:32 -0300

    creating server and route convert files

--- index.js
+++ index.js
@@ -1,29 +1,37 @@
 const cors = require('cors')
 const express = require('express')
 const app = express()
+const PORT = 3000
+
+const libre = require('libreoffice-convert')
+const path = require('path')
+const fs = require('fs')
+
+
+function convertFiles (req,res) {
+  const extend = '.pdf'
+  const enterPath = path.join(__dirname, '/resources/curriculo.docx')
+  const outputPath = path.join(__dirname, `/resources/example${extend}`)
+
+  
+  // Read file
+  const file = fs.readFileSync(enterPath)
+  // Convert it to pdf format with undefined filter (see Libreoffice doc about filter)
+  libre.convert(file, extend, undefined, (err, done) => {
+      if (err) {
+        console.log(`Error converting file: ${err}`)
+      }
+
+      // Here in done you have pdf file which you can save or transfer in another stream
+      fs.writeFileSync(outputPath, done)
+      return res.sendFile(outputPath)
+  })
+}
 
 
 
 
 
-const libre = require('libreoffice-convert');
- 
-const path = require('path');
-const fs = require('fs');
- 
-const extend = '.pdf'
-const enterPath = path.join(__dirname, '/resources/curriculo.docx');
-const outputPath = path.join(__dirname, `/resources/example${extend}`);
-
- 
-// Read file
-const file = fs.readFileSync(enterPath);
-// Convert it to pdf format with undefined filter (see Libreoffice doc about filter)
-libre.convert(file, extend, undefined, (err, done) => {
-    if (err) {
-      console.log(`Error converting file: ${err}`);
-    }
-
-    // Here in done you have pdf file which you can save or transfer in another stream
-    fs.writeFileSync(outputPath, done);
-});
+app.use(cors())
+app.post('/',convertFiles)
+app.listen(PORT, () => console.log(`Server is started in port: ${PORT}`))
